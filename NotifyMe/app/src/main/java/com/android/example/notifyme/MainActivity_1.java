f19afcc29dd0c4728fe9539ab4c03ad7573fef93
duyphaphach <duyphamle97@gmail.com>
2019-04-16 08:37:59 +0700

    Temp

---
+++ NotifyMe/app/src/main/java/com/android/example/notifyme/MainActivity_1.java
@@ -0,0 +1,42 @@
+/*
+ *
+ *   Copyright (C) 2018 Google Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package com.android.example.notifyme;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * MainActivity for the Notify Me! app. Contains three buttons that deliver,
+ * update, and cancel notification.
+ */
+public class MainActivity_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_1);
+    }
+}
